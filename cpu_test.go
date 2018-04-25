1951c237265e841d5df7de7944288d6d69b751d0
Gyanendra Mishra <anomaly.the@gmail.com>
2018-04-25 15:01:57 +0100

    Added a reset function and corrected a few bugs - Program loads from address 512 - Added sad path for incorrect rom - Edit object itself instead of a copy

--- cpu_test.go
+++ cpu_test.go
@@ -14,4 +14,27 @@ func TestLoadProgram(t *testing.T) {
 	c := newCpu()
 	n := c.LoadProgram("roms/PONG")
 	assert.Equal(t, 246, n, "246 bytes should be read as the game is 246 bytes long")
+	for i := 0; i < 512; i++ {
+		assert.Equal(t, uint8(0), c.memory[i], "Should be 0 as first 512 is where emulator resides")
+	}
+}
+
+func TestLoadProgramFailsWithWrongFile(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+
+	c := newCpu()
+	c.LoadProgram("roms/FOO")
+}
+
+func TestReset(t *testing.T) {
+	c := newCpu()
+	c.LoadProgram("roms/PONG")
+	c.I = 42
+	c.Reset()
+	f := newCpu()
+	assert.Equal(t, f, c, "After reset it should be same as new")
 }
